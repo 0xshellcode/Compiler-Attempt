5b1bb711731f5ed39a2c4be2d8d7a481958d4b0d
0xshellcode <38792094+0xshellcode@users.noreply.github.com>
2020-12-03 09:03:30 -0600

    Update emitter.c

--- emitter.c
+++ emitter.c
@@ -9,40 +9,42 @@ emit(int t, int tval) {
   case '-': 
   case '*': 
   case '/':
-    printf("[*] %c \n", t); 
+  case '<':
+  case '>':
+    printf("[*] %c [*]\n", t); 
     break;
   case '=':
-  printf("[*] :=\n");
+  printf("[*] := [*]\n");
     break;  
   case DIV:
-    printf("[*] DIV \n"); 
+    printf("[*] DIV [*]\n"); 
     break;
   case MOD:
-    printf("[*] MOD \n"); 
+    printf("[*] MOD [*]\n"); 
     break;
   case IF:
-    printf("[*] if\n");
+    printf("[*] Proposition -> If [*]\n");
     break;
   case THEN:
-    printf("[*] Then\n");
+    printf("[*] Proposition -> Then [*]\n");
     break;
   case WHILE:
-    printf("[*] While Loop\n");
+    printf("[*] Proposition: Loop -> While [*]\n");
     break;
   case DO:
-    printf("[*] Do Loop\n");
+    printf("[*] Proposition: Loop -> Do [*]\n");
     break;
   case BEGIN:
-    printf("[*] BEGIN\n");
+    printf("[+] BEGIN [+]\n");
     break;
   case END:
-    printf("[*] END\n");
+    printf("[-] END [-]\n");
     break;                      
   case NUM:
-    printf("[*] %d \n", tval); 
+    printf("[*] %d [*]\n", tval); 
     break;
   case ID:
-    printf("[*] %s \n", symtable[tval].lexptr); 
+    printf("[*] %s [*]\n", symtable[tval].lexptr); 
     break;
   default:
     printf("token %d, tokenval %d \n", t, tval);
